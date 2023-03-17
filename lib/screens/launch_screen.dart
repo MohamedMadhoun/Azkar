d5368b85728ea95e0dd9c93d8e3d4580bea4df03
MohamedMadhoun <mohammed.azm2020@gmail.com>
2023-03-17 17:40:26 +0200

    first commit

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, '/home_screen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: AlignmentDirectional.topStart,
+            end: AlignmentDirectional.bottomEnd,
+            colors: [
+              Color(0XFFE38B29),
+              Color(0XFFFFD8A9),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('images/image1.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
